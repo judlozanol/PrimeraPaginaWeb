33e6534afc925fe56487de9f96e331f04c85c88c
Juan Diego Lozano Luna <judlozanol@udistrital.edu.co>
2024-02-13 22:12:16 -0500

    organizacion de las distintas paginas, falta conectar las paginas entre si

---
+++ Cuarto/hola.js
@@ -0,0 +1,28 @@
+function validarPedido(){
+    var form = document.getElementById("FormPedido")
+    var texto= new String("Su pedido de arroz incluye:\n\t Arroz regular.\n");
+    if (document.getElementById('CBCerdo').checked){
+        texto += "\t Porción de Cerdo.\n";
+         
+    }
+    if (document.getElementById('CBRes').checked){
+        texto += "\t Porción de Res.\n";
+    }
+    if (document.getElementById('CBPollo').checked){
+        texto += "\t Porción de Pollo.\n";
+    }
+    if (document.getElementById('CBPinha').checked){
+        texto += "\t Porción de Piña.\n";
+    }
+    texto+="¿Desea continuar con su pedido?"
+    
+    var confirmacion= confirm(texto);
+
+    if (confirmacion){
+        form.submit()
+        alert("Su orden ha sido realizada con exito!")
+    } else {
+        alert("Orden cancelada.")
+    }
+}   
+
